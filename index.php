4fc7fd7218f5d00b4dc9cc9c4d42e58caf7f84c7
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-10-14 12:27:27 -0700

    10/14/14 changes 2:
    Made it HTML with PHP

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <?php
-echo "This is a blog website";
+echo "This is a blog website with Ebola, pls go somewhere else before your PC/Laptop gets it too.";
 ?>
+</body>
+</html>
