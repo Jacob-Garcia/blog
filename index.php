85cb96aff6881360809d40c4151b29e9d758a136
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-10-29 12:00:00 -0700

    10/29/14 Changes 4:
    Added a comment to index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE <!DOCTYPE html>
 <html>
 <head>
-	<title>Blog Website</title>
+	<title>Blog Website</title> 
 </head>
 <body>
 <?php
+/* An action requiring the create-db.php file */
      require_once(__DIR__ . "/controller/create-db.php");
 ?>
 </body>
