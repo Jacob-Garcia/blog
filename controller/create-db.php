7af89f4f4b72ef5803074a1d7fd1f05baabcfd77
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-10-30 11:51:27 -0700

    10/30/14 Changes:
    Redid the table creation in create-db.php to make sure no errors occurred.

--- controller/create-db.php
+++ controller/create-db.php
@@ -13,18 +13,23 @@
     	$query = $connection->query("CREATE DATABASE $database");
     /* An if statement that echoes the creation of the database, now rendered useless */
     	if ($query) {
-    		echo "Successfully created database:" . $database;
+    		echo "Successfully created database: " . $database;
     	}
     }
     /* An else statement that echoes the existing database */
     else {
-    	echo "Database already exists";
+    	echo "Database already exists.";
     }
+
      /* This is a creation of the table posts */
     $query = $connection->query("CREATE TABLE posts ("
-    	    . "id int(11) NOT NULL AUTO_INCREMENT"
-    	    . "title varchar(225) NOT_NULL"
-    	    . "post text NOT NULL,"
-    	    . "PRIMARY KEY (id)");
+    	. "id int(11) NOT NULL AUTO_INCREMENT,"
+    	. "title varchar(255) NOT NULL,"
+    	. "post text NOT NULL,"
+    	. "PRIMARY KEY (id))");
+
+    if ($query) {
+         echo "Successfully created table: posts";
+    }
 
      $connection->close();
