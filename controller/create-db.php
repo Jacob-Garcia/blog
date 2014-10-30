0cd2a5f4bf17ab91dfdf827698a2bc028fada610
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-10-30 12:20:45 -0700

    10/30/14 Changes 4:
    Added more to the comment on line 30 of create-db.php

--- controller/create-db.php
+++ controller/create-db.php
@@ -27,7 +27,7 @@
     	. "title varchar(255) NOT NULL,"
     	. "post text NOT NULL,"
     	. "PRIMARY KEY (id))");
-    /* an if statement that echoes the creation of the table: posts */
+    /* an if statement that echoes the creation of the table: posts. It is not echoing but the tables appear in my myPHPAdmin. */
       if ($query) {
       	echo "Successfully created table: posts";
       }
