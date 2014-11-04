51d78ad956c7c15acd3b2896c15e0faa8531179d
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-04 12:09:48 -0800

    11/4/14 Changes 3:
    Added a comment to navigation.php and deleted text in post.php

--- view/navigation.php
+++ view/navigation.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../model/config.php");
 <nav>
   <ul>
     <li>
-   	  <a href="<?php echo $path . "post.php"; ?>">Blog Post Form</a>
+   	  <a href="<?php echo $path . "post.php";/* Ths echo calls the $path variable, and the "post.php" is called to display it when the link is clicked. */ ?>">Blog Post Form</a>
     </li>
   </ul>
 </nav>
