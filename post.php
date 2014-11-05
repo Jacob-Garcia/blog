c680c3e250342ae3feb1b32feb948929987f72f6
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-05 12:05:47 -0800

    11/5/14 Changes 2:
    Added comments to the form.php and post.php

--- post.php
+++ post.php
@@ -1,4 +1,6 @@
 <?php
+    /*Action requiring the header, form, and footer files to use. */
     require_once(__DIR__ . "/view/header.php");
     require_once(__DIR__ . "/view/form.php");
     require_once(__DIR__ . "/view/footer.php");
+?>
--- view/form.php
+++ view/form.php
@@ -1,16 +1,17 @@
 <h1>Create Blog Post</h1>
 
  <form>
+   <!-- A div that contains the Title label and the input text type. -->
  	 <div>
  	 <label for="title">Title: </label>
  	 <input type="text" name="title"  />
  	 </div>
-
+   <!-- A div that contains the label for Post and the textarea name -->
  	 <div>
  	  <label for="post">Post: </label>
  	  <textarea name="post"></textarea>
  	 </div>
-
+   <!-- a div containing the button used to Submit -->
  	 <div>
  	 	 <button type="submit">Submit</button>
  	 </div>
