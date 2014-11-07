fdc1f3ee99bae9c6cccfb9ed2119b8042cb7eb29
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-07 11:14:48 -0800

    11/7/14 Changes 2:
    Added comments to create-post.php

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,7 @@
 <?php
+   /* Variables that contain the information to recieve blog posts */
    $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
    $post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-
+   /* Echoes to call out the variables */
    echo "<p>Title: $title</p>";
    echo "<p>Post: $post</p>";
