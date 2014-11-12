40c3d4be4e6ffb93a367cae8bbf6fab4d450286a
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-12 12:19:48 -0800

    11/12/14 Changes 4:
    Added comments in create-post.php, a require_once, and a $connection->close();

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,14 @@
 <?php
-   /* Variables that contain the information to recieve blog posts */
-   $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-   $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+     /* A requrie_once that calls for database.php to use. */
+     require_once(__DIR__ . "/../model/database.php");
+     /* A call for the variables titles host, username, password, and database */
+     $connection = new mysqli($host, $username, $password, $database); 
+
+  	 /* Variables that contain the information to recieve blog posts */
+   	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
    /* Echoes to call out the variables */
    echo "<p>Title: $title</p>";
    echo "<p>Post: $post</p>";
+   /*Closes the connection. */
+   $connection->close();
