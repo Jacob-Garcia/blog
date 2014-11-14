536fdef052ccdc846a53470f5be3e96207a75343
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-14 12:15:52 -0800

    11/14/14 Changes 3:
    Added more comments to the Database.php

--- model/Database.php
+++ model/Database.php
@@ -2,7 +2,8 @@
  /* This class was created so it would be easier to call
      all the variables at once, rather than calling them one by one.
      Also, it would be neater and less confusing when referencing
-     to a single class. */
+     to a single class. This class is used to confine the giant block of code
+     within a single Class so it can be referenced as a single object.*/
   class Database {
   	 private $connection;
   	 private $host;
