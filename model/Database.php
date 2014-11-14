d27bf05107021a2d651cb6b17e3b8619d06040a2
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-11-14 12:11:19 -0800

    11/14/14 Changes 2:
    Added a Database class and added a comment.

---
+++ model/Database.php
@@ -0,0 +1,12 @@
+<?php
+ /* This class was created so it would be easier to call
+     all the variables at once, rather than calling them one by one.
+     Also, it would be neater and less confusing when referencing
+     to a single class. */
+  class Database {
+  	 private $connection;
+  	 private $host;
+  	 private $username;
+  	 private $password;
+  	 private $database;
+  }
