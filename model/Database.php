183f5eadf8940cd102d61a4d5b32b368f197392c
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-12-05 12:19:55 -0800

    12/5/14 Changes:
    Added a few blank lines in create-db.php to separate lines of code, added the "<?php" tag to close the php in create-user.php, and added some code in Database.php

--- model/Database.php
+++ model/Database.php
@@ -5,17 +5,20 @@
      to a single class. This class is used to confine the giant block of code
      within a single Class so it can be referenced as a single object.*/
   class Database {
+
   	 private $connection;
   	 private $host;
   	 private $username;
   	 private $password;
   	 private $database;
+     public $error;
 
   	 public function __construct($host, $username, $password, $database) {
             $this->host = $host; /* This is accessing the $host variable, and the rest are accessing the other variables. */
             $this->username = $username;
             $this->password = $password;
             $this->database = $database;
+
   	    $this->connection = new mysqli($host, $username, $password);
 		/* An if statement that kills the code if there is a connection error */
     		if($this->connection->connect_error) {
@@ -32,7 +35,7 @@
     }
     /* An else statement that echoes the existing database */
     	else {
-    		echo "Database already exists.";
+    		echo "<p>Database already exists.</p>";
     }
        }
   	 	public function openConnection() {
@@ -62,6 +65,11 @@
            $this->openConnection();
            /* The variable "$query" is used to use to variable "$this" to create a connection to the "$string" */
            $query = $this->connection->query($string);
+
+           if (!$query) {
+               $this->error = $this->connection->error;
+           }
+
            /* Closes connection to "$this" */
            $this->closeConnection();
            /* Returns the "$query" variable. */
