7d5e53102702d6b65a0f5a93bf9425cd2d0f24ae
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-12-10 11:58:41 -0800

    12/10/14 Changes:
    Added a DateTime thing to the Database and made it use the time in Los Angeles.

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,10 +1,11 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
      /* This is a creation of the table posts */
-    $query = $connection->query("CREATE TABLE posts ("
+    $query = $_SESSION["connection"]->query("CREATE TABLE posts ("
     	. "id int(11) NOT NULL AUTO_INCREMENT,"
     	. "title varchar(255) NOT NULL,"
     	. "post text NOT NULL,"
+      . "DateTime datetime NOT NULL,"
     	. "PRIMARY KEY (id))");
     /* an if statement that echoes the creation of the table: posts. It is not echoing but the tables appear in my myPHPAdmin. */
       if ($query) {
--- controller/create-post.php
+++ controller/create-post.php
@@ -4,13 +4,17 @@
   	 /* Variables that contain the information to recieve blog posts */
    	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-                 
+    $date = new DateTime('today');
+    $time = new DateTime('America/Los_Angeles');
+
     $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
 /* Creates a connection to the query variable */  /* An action that inserts an object into "posts" */
 
    /* an if statement that echoes a line of text to confirm wether or not the $query works. Also has an else statement to echo if it doesn't work. */
     if ($query) {
       echo "<p>Successfully inserted post: $title</p>";
-    } else {
+      echo "Posted on: " . $date->format("M/D/Y") . " at " . $time->format("g:i");
+    } 
+     else {
       echo "<p>$connection->error</p>";
     }
