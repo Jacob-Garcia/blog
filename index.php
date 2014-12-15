1ae64751a89c38dc490bd810bd9eb79899615876
Jacob Garcia <JacobGravestheFiend@gmail.com>
2014-12-15 13:27:39 -0800

    12/15/14 Changes:
    fixed up some code in login-user.php and index.php.

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 	 require_once(__DIR__ . "/controller/login-verify.php");
 /* An action reuqiring the header.php file */
      require_once(__DIR__ . "/view/header.php");
-     
      if (authenticateUser()) {
-     	require_once(__DIR__ . "/view/navigation.php")
+     	require_once(__DIR__ . "/view/navigation.php");
      }
 /* An action requiring the create-db.php file */
      require_once(__DIR__ . "/controller/create-db.php");
